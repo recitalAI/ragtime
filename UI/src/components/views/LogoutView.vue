4d22cdabecfceef74c5ec1b94c60e0675f0f73e8
ImadZaoug <114984003+ImadZaoug@users.noreply.github.com>
2024-09-30 17:49:12 +0200

    Add files via upload

---
+++ UI/src/components/views/LogoutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="logout-view" />
+</template>
+
+<script>
+export default {
+  name: 'LogoutView',
+  mounted() {
+    localStorage.removeItem('user');
+     this.$router.push('/login');
+  }
+}
+</script>
