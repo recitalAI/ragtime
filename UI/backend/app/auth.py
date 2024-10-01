4590414121fdb83b536fa094b7430ac8970fbb44
ImadZaoug <114984003+ImadZaoug@users.noreply.github.com>
2024-10-01 11:51:36 +0100

    Delete UI directory

--- UI/backend/app/auth.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-# Authentication routes can be added here when implemented
--- UI/backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class APIKey(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    value = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.String(50), default='default')  # Using a default value for now
