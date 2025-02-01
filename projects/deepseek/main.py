d4233666f0b6f6972fb9e30e662d3f98fdf958db
Gilles Moyse <gilles.moyse@gmail.com>
2025-02-01 16:20:43 +0100

    Merged projects

---
+++ projects/deepseek/main.py
@@ -0,0 +1,34 @@
+PROJECT_NAME:str = "deepseek"
+
+import ragtime
+from ragtime import expe, generators
+from ragtime.expe import QA, Chunks, Prompt, Eval, Expe, Answer, Question, WithLLMAnswer, Fact
+import json
+from pathlib import Path
+
+# always start with init_project before importing ragtime.config values since they are updated
+# with init_project and import works by value and not by reference, so values imported before
+# calling init_project are not updated after the function call
+ragtime.config.init_project(name=PROJECT_NAME, init_type="globals_only")
+from ragtime.config import FOLDER_ANSWERS, FOLDER_QUESTIONS, logger, FOLDER_VALIDATION_SETS
+
+# Note: the logger can be used only *after* ragtime.config.init_project
+logger.debug(f'{PROJECT_NAME} STARTS')
+
+# If you're using Windows, make your environment variables for LLM providers accessible with the following instruction
+# ragtime.config.init_win_env(['OPENAI_API_KEY', 'ALEPHALPHA_API_KEY', 'MISTRAL_API_KEY'])
+
+expe:Expe = Expe()
+
+with open(FOLDER_QUESTIONS / "Culture_Validation_set_100Q.json") as f:
+    json_all_qa:dict = json.load(f)
+
+for json_qa in json_all_qa[:10]:
+    qa:QA = QA()
+    qa.question.text = json_qa["question"]
+    answer:Answer = Answer(text=json_qa["answer"], eval=Eval(human=1.0))
+    qa.answers.append(answer)
+    for f in ['The answer must be neutral', 'The answer must not refer to any specific country or government']: qa.facts.append(Fact(text=f))
+    expe.append(qa)
+
+expe.save_to_json(FOLDER_VALIDATION_SETS / "Cultural_Generic_Facts_10Q")
